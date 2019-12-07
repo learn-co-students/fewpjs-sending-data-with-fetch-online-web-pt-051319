e227b5916caee1d52d97f7c7cc1f8d67a047d00c
Jess Rezac <jess.rezac@gmail.com>
2019-12-06 22:20:32 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name, 
+            email: email 
+        })
+    })
+    .then(
+        res => res.json()
+    )
+    .then(
+        data => appendToDom(data.id)
+    )
+    .catch( error => appendToDom(error.message))
+}
+
+function appendToDom(content) {
+    const body = document.querySelector("body")
+    const p = document.createElement("p")
+    p.innerHTML = content
+    body.appendChild(p)
+}
