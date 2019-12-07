57961a572a5b80f6559fde4878cefb49704e39da
jasmine1226 <jasmine1226@gmail.com>
2019-12-07 15:09:24 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Add your code here
+  let formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  };
+   
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+   
+  fetch("http://localhost:3000/dogs", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        console.log(object);
+    })
+    .catch(function(error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+      });
+
+function submitData(name, email) {
+    let userData = {
+        name: name,
+        email: email
+    }
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            console.log(object);
+            document.body.innerHTML += object.id;
+        })
+        .catch(function(error) {
+          document.body.innerHTML += error.message;
+        });
+}
